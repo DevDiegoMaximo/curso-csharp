ec62274be169d352e9522dff4d3ba2442ed499fa
Diego Maximo <diego.albuquerque@gurumatch.com.br>
2021-12-27 14:53:38 -0300

    Agpra sim, ta certo

--- Curso/ClassesEMetodos/ParametrosNoemados.cs
+++ Curso/ClassesEMetodos/ParametrosNoemados.cs
@@ -8,9 +8,9 @@ namespace Curso.ClassesEMetodos
     {
         public static void Formatar(int dia, int mes, int ano)
         {
-            Console.WriteLine("{0:D2/{1:D2}/{2}", dia, mes, ano);
-
+            Console.WriteLine("{0:D2}/{1:D2}/{2}", dia, mes, ano);
         }
+
         public static void Executar()
         {
             Formatar(mes: 1, dia: 6, ano: 1996);
